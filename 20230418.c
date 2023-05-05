a535758a0aa8354e22d1b8cc09cb4bb0d35ea359
chammado <kimbklove@gmail.com>
2023-05-06 00:44:57 +0900

    ID: kimbklove@gmail.com		name:202127073

---
+++ 20230418.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int a =5;
+	char b=1;
+	char c=5;
+
+	while(b&c)
+	{
+		printf("%d : %d\n",b,c);
+	
+		b=b>>1;
+		printf("%d : %d\n\n",b,c);
+	}
+
+
+	return 0;
+}
+
